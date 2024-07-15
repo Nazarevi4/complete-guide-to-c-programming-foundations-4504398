ee1bb4bc5d76eb33fc7cac77961baf579449ad39
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-15 09:25:12 +0000

    02_01-string1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH02/02_01/02_01-string1.c
+++ CH02/02_01/02_01-string1.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
-{
+
+int32_t main() {
 	char buffer[32];
 
 	printf("Type your name: ");
-	fgets(buffer,32,stdin);
-	printf("Hello, %s",buffer);
-	return 0;
+	fgets(buffer, 32, stdin);
+	printf("Hello, %s", buffer);
+
+	return EXIT_SUCCESS;
 }

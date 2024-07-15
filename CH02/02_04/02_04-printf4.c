890fb301f9a07b42d83d7ae5b523a2d9bceccc9f
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-15 09:56:00 +0000

    02_04-printf4.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH02/02_04/02_04-printf4.c
+++ CH02/02_04/02_04-printf4.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
-{
+
+int main() {
 	float pi = 3.14159;
 
-	printf("The value of pi: %.2f\n",pi);
-	return 0;
+	printf("The value of pi: %.2f\n", pi);
+
+	return EXIT_SUCCESS;
 }

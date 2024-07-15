8ffe0cbd054bdaa9c12cca49cdb0381927e7d205
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-15 09:36:51 +0000

    02_03-variable1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH02/02_03/02_03-variable1.c
+++ CH02/02_03/02_03-variable1.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
-{
+
+int32_t main() {
 	char a;
 	int b;
 	float c;
@@ -12,10 +14,10 @@ int main()
 	c = 0.25;
 	d = 2.5e6;
 
-	printf("a is %c\n",a);
-	printf("b is %d\n",b);
-	printf("c is %f\n",c);
-	printf("d is %f\n",d);
+	printf("a is %c\n", a);
+	printf("b is %d\n", b);
+	printf("c is %f\n", c);
+	printf("d is %f\n", d);
 
-	return(0);
+	return EXIT_SUCCESS;
 }

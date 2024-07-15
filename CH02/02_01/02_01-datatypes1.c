da2dec8c49437710de7af61cbc2378253731b084
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-15 09:14:38 +0000

    02_01-datatypes1.c - Add stdlib.h, stdint.h. Init the variables. Format the code without affecting the logic.

--- CH02/02_01/02_01-datatypes1.c
+++ CH02/02_01/02_01-datatypes1.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
-{
-	char c;
-	int i;
-	float f;
-	double d;
+
+int32_t main() {
+	char c = '\0';
+	int32_t i = 0;
+	float f = 0.0;
+	double d = 0.0;
 
 	/* input */
 	printf("Type a character: ");
-	scanf("%c",&c);
+	scanf("%c", &c);
 	printf("Type an integer: ");
-	scanf("%d",&i);
+	scanf("%d", &i);
 	printf("Type a small value: ");
-	scanf("%f",&f);
+	scanf("%f", &f);
 	printf("Type a huge value: ");
-	scanf("%lg",&d);
+	scanf("%lg", &d);
 
 	/* output */
-	printf("\nCharacter: %c\n",c);
-	printf("Integer: %d\n",i);
-	printf("Single-precision: %f\n",f);
-	printf("Double-precision: %lg\n",d);
-	return 0;
+	printf("\nCharacter: %c\n", c);
+	printf("Integer: %d\n", i);
+	printf("Single-precision: %f\n", f);
+	printf("Double-precision: %lg\n", d);
+
+	return EXIT_SUCCESS;
 }

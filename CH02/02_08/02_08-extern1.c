bc04968428f1e52d7efbf69ae5bf780c4b4c3913
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-16 18:42:17 +0000

    02_08-extern1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH02/02_08/02_08-extern1.c
+++ CH02/02_08/02_08-extern1.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int a,b;
 
-void sum(void)
-{
-	extern int a,b;
+int32_t a, b;
+
+void sum(void) {
+	extern int32_t a, b;
 
 	a = 91; b = 7;
-	printf("%d + %d = %d\n",a,b,a+b);
+	printf("%d + %d = %d\n", a, b, a + b);
 }
 
-int main()
-{
-	extern int a,b;
+
+int32_t main() {
+	extern int32_t a, b;
 
 	puts("Calling the sum() function:");
 	sum();
 	puts("And in main():");
-	printf("%d + %d = %d\n",a,b,a+b);
+	printf("%d + %d = %d\n", a, b, a + b);
 
-	return(0);
+	return EXIT_SUCCESS;
 }

dcc5e6362d6dc27c4d2624fc1947a0c90028b9da
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-16 15:18:57 +0000

    02_06-challenge1.c - Add solution

--- CH02/02_06/02_06-challenge1.c
+++ CH02/02_06/02_06-challenge1.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
-{
-	printf("Here is the value of A: %d\n",a);
-	printf("Here is the value of B: %c\n",b);
 
-	return(0);
+int32_t main() {
+	int32_t a = 45;
+	const char b = 'F';
+
+	printf("Here is the value of A: %d\n", a);
+	printf("Here is the value of B: %c\n", b);
+
+	return EXIT_SUCCESS;
 }

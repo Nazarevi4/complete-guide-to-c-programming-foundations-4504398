49224d30d1457cd46cdc9bdc5aa1c3127eb4a5d6
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-16 15:05:37 +0000

    02_05-constant4.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH02/02_05/02_05-constant4.c
+++ CH02/02_05/02_05-constant4.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
-{
+
+int32_t main() {
 	const int count = 4;
 
-	printf("Please count to %d\n",count);
-	printf("Do not count to %d\n",count+1);
-	printf("Do not stop at %d\n",count-1);
+	printf("Please count to %d\n", count);
+	printf("Do not count to %d\n", count + 1);
+	printf("Do not stop at %d\n", count - 1);
 
-	return(0);
+	return EXIT_SUCCESS;
 }
-

936a67390e4394f292d32c992ccc6a505b42c1d9
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-16 15:13:56 +0000

    02_05-constant5.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH02/02_05/02_05-constant5.c
+++ CH02/02_05/02_05-constant5.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
 #define MAX 20
 
 /* generate a line */
-void line(int v)
-{
-	int x;
+void line(int32_t v) {
+	int32_t x;
 
-	for( x=0; x<v; x++ )
-	{
-		if( x >= MAX )
+	for (x = 0; x < v; x++) {
+		if (x >= MAX) {
 			break;
+		}
 		putchar('-');
 	}
 	putchar('\n');
 }
 
-int main()
-{
-	int value;
 
-	printf("Enter a positive value less than %d: ",MAX);
-	scanf("%d",&value);
+int32_t main() {
+	int32_t value = 0;
+
+	printf("Enter a positive value less than %d: ", MAX);
+	scanf("%d", &value);
 	line(value);
-	return(0);
+
+	return EXIT_SUCCESS;
 }

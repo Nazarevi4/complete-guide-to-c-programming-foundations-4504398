e5e68770c729767111a205cf55393de0fc5e4da1
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-17 18:40:27 +0000

    02_09-typedef1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH02/02_09/02_09-typedef1.c
+++ CH02/02_09/02_09-typedef1.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
-{
+
+int32_t main() {
 	typedef float radius;
 
 	radius a;
 
 	a = 27.5;
 
-	printf("A circle with radius %.2f has an area of  %.2f\n",
+	printf("A circle with radius %.2f has an area of %.2f\n",
 			a,
-			a*a*3.1415926
-		  );
+			a * a * 3.1415926);
 
-	return(0);
+	return EXIT_SUCCESS;
 }
 

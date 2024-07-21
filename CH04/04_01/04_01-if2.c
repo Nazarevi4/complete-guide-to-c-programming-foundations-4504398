b1704534377ba07d91587b308468f5a7e2fa1514
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-21 14:22:47 +0000

    04_01-if2.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH04/04_01/04_01-if2.c
+++ CH04/04_01/04_01-if2.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
-{
-	int a;
+
+int32_t main() {
+	int32_t a;
 
 	printf("Type a value: ");
-	scanf("%d",&a);
-	if( a < 10 )
-	{
+	scanf("%d", &a);
+	if(a < 10) {
 		puts("You typed a value less than 10");
 		a -= 5;
-		printf("The value is now %d\n",a);
+		printf("The value is now %d\n", a);
 	}
 	puts("Thank you");
 
-	return(0);
+	return EXIT_SUCCESS;
 }

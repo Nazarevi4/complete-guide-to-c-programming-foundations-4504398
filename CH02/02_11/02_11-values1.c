82905ee20ec1aae84d725e414f9f556aa0dd3ba9
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-21 13:52:17 +0000

    02_11-values1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH02/02_11/02_11-values1.c
+++ CH02/02_11/02_11-values1.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
-{
-	int mega = 1000000;				/* no commas! */
+
+int32_t main() {
+	int32_t mega = 1000000;				/* no commas! */
 	float quarter = 0.25;			/* leading zero */
 	double large = 1000000000.0;	/* trailing zero */
 
 	printf("Here are the values:\n");
-	printf("Mega - %d\n",mega);
-	printf("Quarter - %f\n",quarter);
-	printf("Large = %f\n",large);
-	return 0;
+	printf("Mega - %d\n", mega);
+	printf("Quarter - %f\n", quarter);
+	printf("Large = %f\n", large);
+
+	return EXIT_SUCCESS;
 }

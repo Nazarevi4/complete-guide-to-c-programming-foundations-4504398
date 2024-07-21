4817362d1f60d607bd16745a7f80da723dbe8330
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-21 13:55:18 +0000

    02_12-typecast1.c - Add stdint.h. Format the code without affecting the logic.

--- CH02/02_12/02_12-typecast2.c
+++ CH02/02_12/02_12-typecast2.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <time.h>
 
-int main()
-{
-	int x,r[5];
 
-	srand((unsigned)time(NULL));
+int32_t main() {
+	int32_t x, r[5];
 
-	for(x=0;x<5;x++)
-		r[x] = rand()%10+1;
+	srand((uint32_t)time(NULL));
 
-	printf("Here are your %d random values:\n",5);
-	for(x=0;x<5;x++)
-		printf("%3d\n",r[x]);
+	for(x = 0; x < 5; x++) {
+		r[x] = rand() % 10 + 1;
+	}
 
-	return(0);
+	printf("Here are your %d random values:\n", 5);
+	for(x = 0; x < 5; x++) {
+		printf("%3d\n", r[x]);
+	}
+
+	return EXIT_SUCCESS;
 }
-

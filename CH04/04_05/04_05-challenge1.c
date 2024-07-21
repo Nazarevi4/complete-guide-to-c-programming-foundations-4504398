4f85c13820a23b67fde106f473aa3858414ac5d3
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-21 15:31:21 +0000

    04_05-challenge1.c - Add the solution of the challenge

--- CH04/04_05/04_05-challenge1.c
+++ CH04/04_05/04_05-challenge1.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
-{
-	int a;
+
+int32_t main() {
+	int32_t a;
 
 	printf("Select item 1, 2, or 3: ");
-	scanf("%d",&a);
+	scanf("%d", &a);
 
-	if( a == 1 )
+	switch(a) {
+		case 1:
+		  puts("This is the first item");
+		  break;
+		case 2:
+			puts("This is the second item");
+			break;
+		case 3:
+			puts("This is the third item");
+			break;
+		default:
+			puts("Invalid choice!");
+	}
+
+	/*if( a == 1 )
 		puts("This is the first item");
 	else if( a == 2 )
 		puts("This is the second item");
@@ -15,6 +31,7 @@ int main()
 		puts("This is the third item");
 	else
 		puts("Invalid choice!");
+	*/
 
-	return(0);
+	return EXIT_SUCCESS;
 }

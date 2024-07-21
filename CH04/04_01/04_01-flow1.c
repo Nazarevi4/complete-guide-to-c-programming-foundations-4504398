97d31826cbe31f7260bb9b99aff086683f398e95
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-21 14:04:33 +0000

    04_01-flow1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH04/04_01/04_01-flow1.c
+++ CH04/04_01/04_01-flow1.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
-{
+
+int32_t main() {
 	puts("This code runs top-down");
 	puts("Each line executed, one after the other");
 	puts("Up until the function ends");
 	puts("For the main() function, a 'return' ends the flow");
 
-	return 0;
+	return EXIT_SUCCESS;
 }

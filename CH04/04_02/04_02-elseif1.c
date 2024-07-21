535a5310a6828dcf3f60063961f483832ae47e11
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-21 14:40:32 +0000

    04_02-elseif1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH04/04_02/04_02-elseif1.c
+++ CH04/04_02/04_02-elseif1.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
-{
-	int a,b;
+
+int32_t main() {
+	int32_t a, b;
 
 	printf("Enter value 1: ");
-	scanf("%d",&a);
+	scanf("%d", &a);
 	printf("Enter value 2: ");
-	scanf("%d",&b);
-	if( a > b )
-		printf("%d is greater than %d\n",a,b);
-	else if( b > a )
-		printf("%d is greater than %d\n",b,a);
-	else
-		printf("%d is equal to %d\n",a,b);
+	scanf("%d", &b);
 
-	return(0);
+	if(a > b) {
+		printf("%d is greater than %d\n", a, b);
+	}
+	else if( b > a ) {
+		printf("%d is greater than %d\n", b, a);
+	}
+	else {
+		printf("%d is equal to %d\n", a, b);
+	}
+
+	return EXIT_SUCCESS;
 }

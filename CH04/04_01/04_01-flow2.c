c5a0324b05e240f838472287447a0ceb31aa4107
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-21 14:16:11 +0000

    04_01-flow2.c - Add stdint.h. Format the code without affecting the logic.

--- CH04/04_01/04_01-flow2.c
+++ CH04/04_01/04_01-flow2.c
@@ -1,53 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-int get_value()
-{
+
+int32_t get_value() {
 	char buffer[3];
 
-	fgets(buffer,3,stdin);
-	return(atoi(buffer));
+	fgets(buffer, 3, stdin);
+
+	return (atoi(buffer));
 }
 
-void newline(void)
-{
+void newline(void) {
 	putchar('\n');
 }
 
-void separator_row(int w, char c)
-{
-	int x;
-
-	for( x=0; x<w*4; x++ )
+void separator_row(int32_t w, char c) {
+	for(uint32_t x = 0; x < w * 4; x++) {
 		putchar(c);
+	}
 	newline();
 }
 
-int main()
-{
-	int a,b,c,rows;
-	const int columns = 10;
+
+int32_t main() {
+	int32_t a, b, c, rows;
+	const int32_t columns = 10;
 
 	printf("How many rows (18 max)? ");
 	rows = get_value();
-	if( rows>18 )
-		rows= 18;
+	if(rows > 18) {
+		rows = 18;
+	}
 
 	c = 0;
-	for( a=0; a<rows; a++ )
-	{
-		if( a%2 )
-		{
-			separator_row(columns,'=');
+	for(a = 0; a < rows; a++) {
+		if(a % 2) {
+			separator_row(columns, '=');
 		}
-		else
-		{
-			for( b='A'; b<'A'+columns; b++ )
+		else {
+			for(b = 'A'; b < 'A' + columns; b++) {
 				printf(" %d%c ",c,b);
+			}
 			newline();
 			c++;
 		}
 	}
 
-	return 0;
+	return EXIT_SUCCESS;
 }

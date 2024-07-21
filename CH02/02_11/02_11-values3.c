4cd44be8fc86bfa88f8dea46d80324a010f412e1
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-21 13:53:29 +0000

    02_11-values3.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH02/02_11/02_11-values3.c
+++ CH02/02_11/02_11-values3.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
-{
-	int x,o;
+
+int32_t main() {
+	int32_t x, o;
 
 	/* output hexadecimal, base 16 values */
-	for(x=0x1 ; x<=0x10; x++)
-		printf("0x%X = %d\n",x,x);
-	/* output octal, base 8 values */
-	for(o=01 ; o<=010; o++)
-		printf("0%o = %d\n",o,o);
+	for(x = 0x1 ; x <= 0x10; x++) {
+		printf("0x%X = %d\n", x, x);
+	}
+	putchar('\n');
 
-	return(0);
+	/* output octal, base 8 values */
+	for(o = 01 ; o <= 010; o++) {
+		printf("0%o = %d\n", o, o);
+	}
+
+	return EXIT_SUCCESS;
 }

87fe3a039f85ad762624f8d27787441f2b4da3cc
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-21 18:02:09 +0000

    04_07-for3.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH04/04_07/04_07-for3.c
+++ CH04/04_07/04_07-for3.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
-{
-	int a,b;
 
-	for( a=1, b=10 ; a<=10 ; a++, b-- )
-		printf("%2d %2d\n",a,b);
+int32_t main() {
+	uint8_t a, b;
 
-	return(0);
+	for(a = 1, b = 10 ; a <= 10 ; a++, b--) {
+		printf("%2d %2d\n", a, b);
+	}
+
+	return EXIT_SUCCESS;
 }
-

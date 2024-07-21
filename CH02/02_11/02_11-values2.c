50594cbf199ea464987aa9e6aa3711c04bad55e4
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-21 13:53:13 +0000

    02_11-values2.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH02/02_11/02_11-values2.c
+++ CH02/02_11/02_11-values2.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
-{
-	printf("%f\n",2.5E6);
-	printf("%f\n",2.5e-6);
 
-	return(0);
+int32_t main() {
+	printf("%f\n", 2.5E6);
+	printf("%f\n", 2.5e-6);
+
+	return EXIT_SUCCESS;
 }

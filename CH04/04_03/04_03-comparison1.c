4358d0b1fe4f7470afbdea92df8b401daed9efa9
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-21 15:00:52 +0000

    04_03-comparison1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH04/04_03/04_03-comparison1.c
+++ CH04/04_03/04_03-comparison1.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
-{
-	int a,b,c;
+
+int32_t main() {
+	int32_t a, b, c;
 
 	a = 10;
 	b = 14;
-	if( a > b )
-		c = a;
-	else
-		c = b;
-	printf("%d is the greater of %d and %d\n",c,a,b);
 
-	return(0);
+	if(a > b) {
+		c = a;
+	}
+	else {
+		c = b;
+	}
+
+	printf("%d is the greater of %d and %d\n", c, a, b);
+
+	return EXIT_SUCCESS;
 }

3a0e0202f4e360b63567d7011934d99bdbb2b47c
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-21 13:50:39 +0000

    02_11-inputtest1.c - Add stdint.h. Format the code without affecting the logic.

--- CH02/02_11/02_11-inputtest1.c
+++ CH02/02_11/02_11-inputtest1.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-int main()
-{
+
+int32_t main() {
 	char buffer[16];
-	int lv;
+	int32_t lv = 0;
 
 	printf("Type a large value: ");
-	fgets(buffer,16,stdin);
-	lv = strtol(buffer,NULL,10);
-	printf("You typed %d\n",lv);
-	return 0;
+	fgets(buffer, 16, stdin);
+	lv = strtol(buffer, NULL, 10);
+	printf("You typed %d\n", lv);
+
+	return EXIT_SUCCESS;
 }

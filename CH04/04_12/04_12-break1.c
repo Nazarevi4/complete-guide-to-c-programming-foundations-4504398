32e713fba086ef3b519c6fff841ed045ab73ac1e
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-22 07:32:23 +0000

    04_12-break1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH04/04_12/04_12-break1.c
+++ CH04/04_12/04_12-break1.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
-{
-	int a,b;
+
+int32_t main() {
+	int32_t b = 0;
 
 	printf("Type a positive value: ");
-	scanf("%d",&b);
-	for(a=0;a<b;a++)
-	{
-		printf("I must do this %d times\n",b);
-		if(a==9)
-			break;
-	}
-	
-	return(0);
-}
+	scanf("%d", &b);
 
+	for(uint8_t a = 0; a < b; a++) {
+		printf("I must do this %d times\n", b);
+		if(a == 9) { break; }
+	}
+
+	return EXIT_SUCCESS;
+}

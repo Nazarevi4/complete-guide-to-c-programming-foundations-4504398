39f9b9daa9910f8df3d16b390f5f61ffe6671ff6
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-22 07:53:26 +0000

    04_13-goto1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH04/04_13/04_13-goto1.c
+++ CH04/04_13/04_13-goto1.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
-{
-	int a = 10;
+
+int32_t main() {
+	int32_t a = 10;
 
 here:
-	printf("%d\n",a);
+	printf("%d\n", a);
 	a--;
-	if( a > 0 )
+
+	if(a > 0) {
 		goto here;
+	}
 
-	return(0);
+	return EXIT_SUCCESS;
 }
-

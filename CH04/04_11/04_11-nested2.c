c72cf6be24ad9c59c5aa19aef2205f7be0b4b7ee
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-22 07:14:00 +0000

    04_11-nested2.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH04/04_11/04_11-nested2.c
+++ CH04/04_11/04_11-nested2.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
-{
-	int row;
+
+int32_t main() {
+	uint8_t row;
 	char column;
 
 	row = 1;
-	while(row<=10)
-	{
+	while(row <= 10) {
 		column = 'A';
-		while(column<='J')
-		{
-			printf("%2d%c   ",row,column);
-			column++;
+		while(column<='J') {
+			printf("%2d%c   ", row, column++);
+			//column++;
 		}
 		putchar('\n');
 		row++;
 	}
 
-	return(0);
+	return EXIT_SUCCESS;
 }

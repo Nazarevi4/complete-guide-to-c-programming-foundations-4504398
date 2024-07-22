f6619f5578c7131cb425b16950d424fadabe236d
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-22 07:41:15 +0000

    04_12-continue1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH04/04_12/04_12-continue1.c
+++ CH04/04_12/04_12-continue1.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
-{
-	int a;
+
+int32_t main() {
+	int32_t a;
 
 	a = 0;
-	while(1)
-	{
+	while(1) {
 		a++;
-		printf("%d\n",a);
-		if( a < 10 )
+		printf("%d\n", a);
+		if(a < 10) {
 			continue;
+		}
+
 		a++;
-		if( a > 20 )
+		if(a > 20) {
 			break;
+		}
 	}
 
-	return(0);
+	return EXIT_SUCCESS;
 }

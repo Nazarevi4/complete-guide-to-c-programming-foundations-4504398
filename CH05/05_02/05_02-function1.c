1bc09089379d5fcb003634b2a81ad4d519cb1ffd
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-23 14:00:44 +0000

    05_02-function1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH05/05_02/05_02-function1.c
+++ CH05/05_02/05_02-function1.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
 
 void sum(void);
 
-int main()
-{
+
+int32_t main() {
 	puts("Calling the sum() function:");
 	sum();
 	puts("Done");
 
-	return(0);
+	return EXIT_SUCCESS;
 }
 
-void sum(void)
-{
-	int a,b,sum;
+
+void sum(void) {
+	int32_t a, b, sum;
 
 	a = 91; b = 7;
 	sum = a + b;
-	printf("%d + %d = %d\n",a,b,sum);
+	printf("%d + %d = %d\n", a, b, sum);
 }
-

c16b6aa91d9de2dbd9529c46b5eca6cea946cfb6
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-23 13:53:53 +0000

    05_01-functypes1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH05/05_01/05_01-functypes1.c
+++ CH05/05_01/05_01-functypes1.c
@@ -1,37 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-char valchar(void)
-{
+
+char valchar(void) {
 	return 'A';
 }
 
-int valint(void)
-{
+int32_t valint(void) {
 	return 7;
 }
 
-float valfloat(void)
-{
+float valfloat(void) {
 	return 3.141;
 }
 
-double valdouble(void)
-{
+double valdouble(void) {
 	return 6.023e23;
 }
 
-void valvoid(void)
-{
+void valvoid(void) {
 	puts("void function returns nothing!");
 }
 
-int main()
-{
-	printf("char function returns %c\n",valchar() );
-	printf("int function returns %d\n",valint() );
-	printf("float function returns %f\n",valfloat() );
-	printf("double function returns %f\n",valdouble() );
+
+int32_t main() {
+	printf("char function returns %c\n", valchar());
+	printf("int function returns %d\n", valint());
+	printf("float function returns %f\n", valfloat());
+	printf("double function returns %f\n", valdouble());
 	valvoid();
 
-	return 0;
+	return EXIT_SUCCESS;
 }

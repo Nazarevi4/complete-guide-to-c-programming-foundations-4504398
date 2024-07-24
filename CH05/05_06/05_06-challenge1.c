410347df56bab331d11093069c600733ef45b7ca
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-24 07:08:30 +0000

    05_06-challenge1.c - Add the solution of the challenge

--- CH05/05_06/05_06-challenge1.c
+++ CH05/05_06/05_06-challenge1.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <time.h>
 
-int main()
-{
-	int r,dice1,dice2,dice3;
+
+int32_t roll(void);
+
+
+int32_t main() {
+	int32_t dice1, dice2, dice3;
 
 	/* seed the randomizer */
-	srand( (unsigned)time(NULL) );
+	srand((unsigned)time(NULL));
 	
 	puts("Roll them bones!");
-	r = rand() % 6;
-	r += 1;
-	dice1 = r;
-	r = rand() % 6;
-	r += 1;
-	dice2 = r;
-	r = rand() % 6;
-	r += 1;
-	dice3 = r;
-	printf("You rolled %d - %d - %d\n",dice1,dice2,dice3);
+	dice1 = roll();
+	dice2 = roll();
+	dice3 = roll();
+	printf("You rolled %d - %d - %d\n", dice1, dice2, dice3);
 
-	return(0);
+	return EXIT_SUCCESS;
+}
+
+
+int32_t roll(void) {
+	return (rand() % 6 + 1);
 }

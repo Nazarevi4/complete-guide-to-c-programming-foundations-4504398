fae04cbe7870086c76eac3ab2017311a49370388
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-24 06:58:57 +0000

    05_05-function2.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH05/05_05/05_05-function2.c
+++ CH05/05_05/05_05-function2.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <time.h>
 
-void greetings(void)
-{
+
+void greetings(void) {
 	time_t now;
 	struct tm *today;
 
 	time(&now);
 	today = localtime(&now);
-	if( today->tm_hour > 16 )
-	{
+	if(today->tm_hour > 16) {
 		puts("Good Evening");
 		return;
 	}
 	puts("It's not evening");
 }
 
-int main()
-{
+
+int32_t main() {
 	greetings();
 
-	return(0);
+	return EXIT_SUCCESS;
 }

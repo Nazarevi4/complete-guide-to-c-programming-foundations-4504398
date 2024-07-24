4ba59614d9350ab35484ec17fb453704b5ed81cf
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-24 07:15:09 +0000

    05_08-function2.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH05/05_08/05_08-function2.c
+++ CH05/05_08/05_08-function2.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-float average(float a, float b, float c)
-{
+
+float average(float a, float b, float c) {
 	float avg;
 
 	avg = ( a + b + c ) / 3;
 
-	return(avg);
+	return (avg);
 }
 
-int main()
-{
+
+int32_t main() {
 	float a;
 
 	a = average( 29.0, 37.0, 30.2 );
-	printf("The average of 29.0, 37.0 and 30.2 is %.2f\n",a);
+	printf("The average of 29.0, 37.0 and 30.2 is %.2f\n", a);
 
-	return(0);
+	return EXIT_SUCCESS;
 }
-

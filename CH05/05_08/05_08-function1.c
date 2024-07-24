e58aba70c298db3cefacd01c587505562e71c243
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-24 07:13:25 +0000

    05_08-function1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH05/05_08/05_08-function1.c
+++ CH05/05_08/05_08-function1.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-void repeat(int count)
-{
-	int x;
 
-	for(x=0;x<count;x++)
+void repeat(uint32_t count) {
+	for(uint32_t x = 0; x < count; x++) {
 		printf("Blah ");
+	}
 	putchar('\n');
 }
 
-int main()
-{
+
+int32_t main() {
 	printf("Chris said: ");
 	repeat(1);
 	printf("Pat said: ");
 	repeat(3);
 
-	return(0);
+	return EXIT_SUCCESS;
 }

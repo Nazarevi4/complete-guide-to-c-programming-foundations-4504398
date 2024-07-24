111f20133e570643724e9feb37275f88673b46ae
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-24 07:34:46 +0000

    05_09-challenge1.c - Add the solution of the challenge

--- CH05/05_09/05_09-challenge1.c
+++ CH05/05_09/05_09-challenge1.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
-{
+
+void line(uint32_t length, char ch);
+
+
+int32_t main() {
 	puts("Report on Extraterrestrial Activity");
-	line(35,'-');
+	line(35, '-');
 
-	return(0);
+	return EXIT_SUCCESS;
+}
+
+
+void line(uint32_t length, char ch) {
+	for(; length > 0; length--) {
+		putchar(ch);
+	}
+	putchar('\n');
 }

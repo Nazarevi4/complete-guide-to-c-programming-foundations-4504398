0e1f7da188de299f22194dea96771f2024f58e75
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-24 07:47:30 +0000

    05_11-arguments1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH05/05_11/05_11-arguments1.c
+++ CH05/05_11/05_11-arguments1.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main(int argc, char *argv[])
-{
-	int x;
 
-	printf("%d arguments found:\n",argc);
-	for( x=0; x<argc; x++ )
-		printf("\t%d: %s\n",x,argv[x]);
-	return 0;
+int32_t main(int32_t argc, char *argv[]) {
+	int32_t x;
+
+	printf("%d arguments found:\n", argc);
+	for(x = 0; x < argc; x++) {
+		printf("\t%d: %s\n", x, argv[x]);
+	}
+
+	return EXIT_SUCCESS;
 }

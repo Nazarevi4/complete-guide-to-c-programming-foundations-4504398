ddf040888bdae21151256dd735504bea64c6e8d8
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-26 07:26:51 +0000

    05_13-recursive2.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH05/05_13/05_13-recursive2.c
+++ CH05/05_13/05_13-recursive2.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-void recurse(int a)
+
+void recurse(int32_t a)
 {
 	a++;
-	if( a>10 )
-		return;
-	printf("%d\n",a);
+	if( a > 10 ) { return; }
+
+	printf("%d\n", a);
 	recurse(a);
 }
 
-int main()
+
+int32_t main()
 {
 	recurse(0);
 
-	return 0;
+	return EXIT_SUCCESS;
 }

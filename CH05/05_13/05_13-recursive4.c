f966e6ab1295afcd1a333c224a5a01160857d0d5
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-26 07:27:31 +0000

    05_13-recursive4.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH05/05_13/05_13-recursive4.c
+++ CH05/05_13/05_13-recursive4.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int factorial(int f)
+
+int32_t factorial(int32_t f)
 {
-	if( f == 1 )
-		return(f);
-	return( f * factorial( f-1 ) );
+	if( f == 1 ) { return(f); }
+
+	return ( f * factorial( f-1 ) );
 }
 
-int main()
+
+int32_t main()
 {
-	int a,b;
+	int32_t a, b;
 
 	printf("Enter a positive value: ");
-	scanf("%d",&a);
+	scanf("%d", &a);
 	if( a < 1 )
 	{
-		printf("%d is not a positive value\n",a);
-		return(1);
+		printf("%d is not a positive value\n", a);
+		return (1);
 	}
-	b = factorial(a);
-	printf("The factorial of %d! is %d\n",a,b);
 
-	return(0);
+	b = factorial(a);
+	printf("The factorial of %d! is %d\n", a, b);
+
+	return EXIT_SUCCESS;
 }

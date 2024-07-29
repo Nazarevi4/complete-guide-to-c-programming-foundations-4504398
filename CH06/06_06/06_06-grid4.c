b29d0b42dd886e44c2568e4e32910d0611fc25bf
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-29 08:05:55 +0000

    06_06-grid4.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH06/06_06/06_06-grid4.c
+++ CH06/06_06/06_06-grid4.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
+
+int32_t main()
 {
 	char tictactoe[27];
-	int x,y,z;
+	int32_t x, y, z;
 
 	/* initialize */
-	for(x=0; x<27; x++ )
+	for(x = 0; x < 27; x++ )
+	{
 		tictactoe[x] = '.';
+	}
 
 	/* display */
 	puts("3D Tic Tac Toe");
-	for(x=0;x<3;x++)
+	for(x = 0; x < 3; x++)
 	{
-		printf("%*c",x+1,' ');
-		for(y=0;y<3;y++)
+		printf("%*c", x + 1, ' ');
+		for(y = 0; y < 3; y++)
 		{
-			for(z=0;z<3;z++)
-				printf(" %c ",tictactoe[x*9+y*3+z]);	/* math required to accurately plot out elements */
+			for(z = 0; z < 3; z++)
+			{
+				printf(" %c ", tictactoe[x * 9 + y * 3 + z]);	/* math required to accurately plot out elements */
+			}
 			printf("     ");	/* 5 spaces */
 		}
 		putchar('\n');
 	}
 
-	return(0);
+	return EXIT_SUCCESS;
 }

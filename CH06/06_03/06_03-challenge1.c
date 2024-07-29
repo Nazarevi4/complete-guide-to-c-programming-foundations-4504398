c396edaa6549706c04a42c974026dcd3ff01310c
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-29 07:26:13 +0000

    06_03-challenge1.c - Add the solution of the challenge

--- CH06/06_03/06_03-challenge1.c
+++ CH06/06_03/06_03-challenge1.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
+
+int32_t main()
 {
-	return 0;
+	int32_t age[5] = {45, 23, 7, 83, 45};
+
+	for(uint8_t x = 0; x < 5; x++)
+	{
+		printf("I know someone who is %d years old\n", age[x]);
+	}
+
+	return EXIT_SUCCESS;
 }

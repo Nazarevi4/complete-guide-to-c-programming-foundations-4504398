3dfd04da06391182bc8534ee736472a551532267
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-29 08:05:00 +0000

    06_06-grid1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH06/06_06/06_06-grid1.c
+++ CH06/06_06/06_06-grid1.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
+
+int32_t main()
 {
-	int grid[5][2];
-	int x;
+	int32_t grid[5][2];
+	int32_t x;
 
-	for(x=0;x<5;x++)
+	for(x = 0; x < 5; x++)
 	{
-		grid[x][0] = x+1;
+		grid[x][0] = x + 1;
 		grid[x][1] = grid[x][0] * 10;
 	}
 
-	for(x=0;x<5;x++)
-		printf("%d,%d\n",grid[x][0],grid[x][1]);
+	for(x = 0; x < 5; x++)
+	{
+		printf("%d,%d\n", grid[x][0], grid[x][1]);
+	}
 
-	return(0);
+	return EXIT_SUCCESS;
 }

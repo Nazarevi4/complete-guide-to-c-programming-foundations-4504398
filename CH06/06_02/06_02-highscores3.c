70e1dba4da8dbae86aa4dadfe215cf1db2c82cbe
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-29 07:10:40 +0000

    06_02-highscores3.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH06/06_02/06_02-highscores3.c
+++ CH06/06_02/06_02-highscores3.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
+
+int32_t main()
 {
 	float highscore[5] = { 993.86, 682.01, 639.60, 310.45, 123.25 };
-	int x;
+	int32_t x;
 
 	puts("Here are the high scores:");
-	for(x=0;x<5;x++)
-		printf("#%d %.2f\n",x+1,highscore[x]);
+	for(x = 0; x < 5; x++) {
+		printf("#%d %.2f\n", x + 1, highscore[x]);
+	}
 
-	return(0);
+	return EXIT_SUCCESS;
 }
-

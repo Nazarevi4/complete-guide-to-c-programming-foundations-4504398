8590b2dc4c238f89d5f0bf5bbc2ae78ac21a7d57
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-29 08:49:52 +0000

    06_09-challenge1.c - Add the solution of the challenge

--- CH06/06_09/06_09-challenge1.c
+++ CH06/06_09/06_09-challenge1.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
+
+int32_t main()
 {
-	return 0;
+	struct date	{
+		uint32_t month;
+		uint32_t day;
+		uint32_t year;
+	};
+
+	struct person {
+		struct date birthday;
+		float height;
+		char name[32];
+	} John = {
+		{06, 21, 1994},
+		(1.92 / 1000),
+		"John"};
+	
+	printf("%s was born on %d/%d/%d and is %f kilometers tall\n",
+	    John.name,
+			John.birthday.month,
+	    John.birthday.day,
+			John.birthday.year,
+			John.height);
+
+	return EXIT_SUCCESS;
 }

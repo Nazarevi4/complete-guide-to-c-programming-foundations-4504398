e50a2345fe3cc1aa42865bdc5f5c6a9eeeb6f83e
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-29 08:31:19 +0000

    06_08-nested1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH06/06_08/06_08-nested1.c
+++ CH06/06_08/06_08-nested1.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 
-int main()
+
+int32_t main()
 {
 	struct date {
-		int month;
-		int day;
-		int year;
+		int32_t month;
+		int32_t day;
+		int32_t year;
 	};
+
 	struct person {
 		struct date birthday;
 		char name[32];
 	};
+
 	struct person president;
 
 	president.birthday.month = 2;
 	president.birthday.day = 22;
 	president.birthday.year = 1732;
-	strcpy(president.name,"George Washington");
+	strcpy(president.name, "George Washington");
 
 	printf("%s was born on %d/%d/%d\n",
 			president.name,
 			president.birthday.month,
 			president.birthday.day,
-			president.birthday.year
-		  );
+			president.birthday.year);
 
-	return(0);
+	return EXIT_SUCCESS;
 }
-

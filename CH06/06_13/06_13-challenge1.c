7dc0a66353f74353b542699475af163b50a3d126
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-30 08:20:25 +0000

    06_13-challenge1.c - Add the solution of the challenge

--- CH06/06_13/06_13-challenge1.c
+++ CH06/06_13/06_13-challenge1.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
 
 #define SIZE 32
 
@@ -8,11 +10,12 @@ void output(struct person y);
 
 struct person {
 	char name[SIZE];
-	int id;
+	int32_t id;
 };
 
+
 /* main program */
-int main()
+int32_t main()
 {
 	struct person you;
 
@@ -22,13 +25,27 @@ int main()
 	/* output the structure */
 	output(you);
 
-	return 0;
+	return EXIT_SUCCESS;
 }
+
 
 struct person fill(void)
 {
+	struct person customer;
+
+	printf("Enter your name: ");
+	fgets(customer.name, SIZE, stdin);
+
+	printf("Enter your ID: ");
+	scanf("%d", &customer.id);
+
+	return customer;
 }
+
 
 void output(struct person y)
 {
+	printf("ID %d belongs to %s",
+	    y.id,
+			y.name);
 }

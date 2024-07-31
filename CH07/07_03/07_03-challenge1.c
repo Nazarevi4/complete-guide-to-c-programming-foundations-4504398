6a023588a938f754ebc8e94740656bb36b928d33
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-31 08:13:56 +0000

    07_03-challenge1.c - Add the solution of the challenge

--- CH07/07_03/07_03-challenge1.c
+++ CH07/07_03/07_03-challenge1.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <ctype.h>
 
-int main()
+
+int32_t main()
 {
 	char string[] = "Nifty TEXT! 123\n";
+	int32_t ch;
+	int32_t k = 0;
 
-	return(0);
+	while( (ch = string[k]) != '\0' )
+	{
+		if( isupper(ch) )
+		{
+			putchar( tolower(ch) );
+		}
+		else if( islower(ch) )
+		{
+			putchar( toupper(ch) );
+		}
+		else
+		{
+			putchar(ch);
+		}
+
+		k++;
+	}
+
+	return EXIT_SUCCESS;
 }

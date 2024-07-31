5499e34c794df346538a548ae09b21cf11703690
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-31 07:57:44 +0000

    07_02-testchar3.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH07/07_02/07_02-testchar3.c
+++ CH07/07_02/07_02-testchar3.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <ctype.h>
 
-int main()
+
+int32_t main()
 {
 	char string[] = "Nifty TEXT! 123\n";
 	char ch;
-	int x = 0;
+	int32_t x = 0;
 
 	while( (ch = string[x]) != '\0' )
 	{
 		if( isupper(ch) )
+		{
 			putchar('_');
+		}
 		else
+		{
 			putchar(ch);
+		}
 		x++;
 	}
 
-	return(0);
+	return EXIT_SUCCESS;
 }

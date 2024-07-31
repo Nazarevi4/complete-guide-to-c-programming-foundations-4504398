85468fc579c786c1f301a37ee89a0cab8e604254
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-31 12:03:23 +0000

    07_07-printf1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH07/07_07/07_07-printf1.c
+++ CH07/07_07/07_07-printf1.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
+
+int32_t main()
 {
 	char string[] = "Yet another string literal";
 
 	printf("A string literal");
 	printf(string);
 
-	return(0);
+	return EXIT_SUCCESS;
 }
-

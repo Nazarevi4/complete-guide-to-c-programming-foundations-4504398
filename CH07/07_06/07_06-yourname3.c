834ebf2bed83e225e9d4e3c416f792ebaeb54505
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-07-31 09:03:06 +0000

    07_06-yourname3.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH07/07_06/07_06-yourname3.c
+++ CH07/07_06/07_06-yourname3.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
+
+int32_t main()
 {
 	char input[10];
-	int i;
 
 	printf("Your name? ");
-	fgets(input,10,stdin);
-	for(i=0;i<10;i++)
+	fgets(input, 10, stdin);
+
+	for(int8_t i = 0; i < 10; i++)
 	{
 		if(input[i] == '\n')
+		{
 			input[i] = '\0';
+		}
 	}
-	printf("Pleased to meet you, %s!\n",input);
+	printf("Pleased to meet you, %s!\n", input);
 
-	return(0);
+	return EXIT_SUCCESS;
 }
-

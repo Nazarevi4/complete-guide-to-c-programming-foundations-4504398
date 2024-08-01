5fa5671a67e72ffa389266bbffc6ac9f173098e6
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-01 09:02:39 +0000

    07_12-challenge1.c - Add the solution of the challenge

--- CH07/07_12/07_12-challenge1.c
+++ CH07/07_12/07_12-challenge1.c
@@ -1,22 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <ctype.h>
 #include <string.h>
 
+
+#define SIZE  32
+
+
 /* main program */
-int main()
+int32_t main()
 {
 	// variable declaration
+	char input_command[SIZE];
 
 	while(1)
 	{
 		printf("Command: ");
 		// fetch input
+		fgets(input_command, SIZE, stdin);
+
 		// remove newline
-		// convert to uppercase
+		for(uint32_t k = 0; input_command[k] != '\0'; k++)
+		{
+			if(input_command[k] == '\n') { input_command[k] = '\0'; }
+
+			if( isalpha(input_command) )
+			{ // convert to uppercase
+			    input_command[k] = toupper(input_command[k]);
+			}
+		}
+
 		// output "Processing command 'cmd'"
+		printf("Processing command %s\n", input_command);
+
 		// break the loop on `QUIT`
+		if(strcmp(input_command, "QUIT") == 0) { break; }
 	}
 
-	return 0;
+	return EXIT_SUCCESS;
 }

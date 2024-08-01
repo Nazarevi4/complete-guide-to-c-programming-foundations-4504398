cc42be0b11a3faa79fe9ea7953d2667686017fdf
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-01 08:25:34 +0000

    07_10-strcmp1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH07/07_10/07_10-strcmp1.c
+++ CH07/07_10/07_10-strcmp1.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 
-int main()
+
+int32_t main()
 {
 	char password[] = "secret\n";
 	char input[10];
 
 	printf("Password: ");
-	fgets(input,10,stdin);
+	fgets(input, 10, stdin);
 
-	if( strcmp(password,input)==0 )
+	if( strcmp(password,input) == 0 )
+	{
 		puts("Entry granted!");
+	}
 	else
+	{
 		puts("Incorrect password!");
+	}
 
-	return(0);
+	return EXIT_SUCCESS;
 }
-

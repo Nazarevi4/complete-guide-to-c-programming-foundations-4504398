a041ea86d20d5d579a4a1352facc4a61fe8a725b
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-01 08:32:49 +0000

    07_11-strbuild3.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH07/07_11/07_11-strbuild3.c
+++ CH07/07_11/07_11-strbuild3.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 
-int main()
+
+int32_t main()
 {
 	char first[16] = "George";
 	char last[16] = "Washington";
 	char full[32];
 
 	/* build the string */
-	strcpy(full,first);
+	strcpy(full, first);
 	strcat(full," ");
-	strcat(full,last);
+	strcat(full, last);
 
-	printf("Pleased to meet you, %s\n",full);
+	printf("Pleased to meet you, %s\n", full);
 
-	return(0);
+	return EXIT_SUCCESS;
 }

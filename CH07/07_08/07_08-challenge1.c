e070f474fa3d49bd4af72588281755279f4a47ff
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-01 08:11:58 +0000

    07_08-challenge1.c - Add the solution of the challenge

--- CH07/07_08/07_08-challenge1.c
+++ CH07/07_08/07_08-challenge1.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
+
+int32_t main()
 {
-	return 0;
+	char string[] = "Jeans";
+	uint32_t quantity = 15;
+	float price = 19.99;
+
+	printf("You have purchased %d %s at $%.2f each.\n",
+	    quantity,
+			string,
+			price);
+
+	printf("The total cost is $%.2f\n", (price * quantity));
+
+			return EXIT_SUCCESS;
 }

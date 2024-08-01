62aad6180b54337a4e0fd10dad502e502a9ebe31
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-01 08:17:54 +0000

    07_10-strlen1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH07/07_10/07_10-strlen1.c
+++ CH07/07_10/07_10-strlen1.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 
-int main()
+
+int32_t main()
 {
 	char string[] = "Hello, you happy planet.";
-	int len;
+	int32_t len;
 
 	len = strlen(string);
-	printf("The string \"%s\" is %d characters long.\n",string,len);
+	printf("The string \"%s\" is %d characters long.\n", string, len);
 
-	return(0);
+	return EXIT_SUCCESS;
 }
-

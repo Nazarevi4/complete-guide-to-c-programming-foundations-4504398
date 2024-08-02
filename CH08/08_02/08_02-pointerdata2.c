97a569bcf6eb3844ea8269a1482bc1020180d147
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-02 07:23:50 +0000

    08_02-pointerdata2.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH08/08_02/08_02-pointerdata2.c
+++ CH08/08_02/08_02-pointerdata2.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
+
+int32_t main()
 {
-	int alpha;
-	int *ptr;
+	int32_t alpha;
+	int32_t *ptr;
 
 	alpha = 99;
 	ptr = &alpha;
-	printf("The address of variable alpha is %p\n",&alpha);
-	printf("The value stored in pointer ptr is %p\n",ptr);
-	printf("The content of variable alpha is %d\n",alpha);
-	printf("The content of memory address %p is %d\n",ptr,*ptr);
+	printf("The address of variable alpha is %p\n", &alpha);
+	printf("The value stored in pointer ptr is %p\n", ptr);
+	printf("The content of variable alpha is %d\n", alpha);
+	printf("The content of memory address %p is %d\n", ptr, *ptr);
 
-	return(0);
+	return EXIT_SUCCESS;
 }

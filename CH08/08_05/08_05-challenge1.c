f46fbe13dd9abd838dd005c2d3b45debcc425635
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-02 08:16:51 +0000

    08_05-challenge1.c - Add the solution of the challenge

--- CH08/08_05/08_05-challenge1.c
+++ CH08/08_05/08_05-challenge1.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
+
+int32_t main()
 {
 	char title[] = "Pointers don't intimidate me!\n";
+	char *text_ptr = title;
+	uint32_t k = 0;
 
-	return 0;
+	/* Solution 1 */
+	while ( *(text_ptr + k) != '\0')
+	{
+		putchar(*(text_ptr + k++));
+	}
+
+	/* Solution 2 */
+	while( *text_ptr != '\0')
+	{
+		putchar(*(text_ptr)++);
+	}
+
+	return EXIT_SUCCESS;
 }

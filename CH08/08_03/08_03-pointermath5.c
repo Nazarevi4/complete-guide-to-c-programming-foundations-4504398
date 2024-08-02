7c6383613d3f1716395310f5f24b4b63c4d20f12
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-02 07:46:15 +0000

    08_03-pointermath5.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH08/08_03/08_03-pointermath5.c
+++ CH08/08_03/08_03-pointermath5.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
+
+int32_t main()
 {
 	printf("Size of variable storage:\n");
-	printf("char\t%lu\n",sizeof(char));
-	printf("int\t%lu\n",sizeof(int));
-	printf("float\t%lu\n",sizeof(float));
-	printf("double\t%lu\n",sizeof(double));
+	printf("char\t%lu\n", sizeof(char));
+	printf("int\t%lu\n", sizeof(int32_t));
+	printf("float\t%lu\n", sizeof(float));
+	printf("double\t%lu\n", sizeof(double));
 
-	return(0);
+	return EXIT_SUCCESS;
 }

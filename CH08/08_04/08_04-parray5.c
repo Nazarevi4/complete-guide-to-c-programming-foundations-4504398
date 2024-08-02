c5866f408697eab31f8b343568bd92df8a5ea5a1
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-02 08:03:00 +0000

    08_04-parray5.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH08/08_04/08_04-parray5.c
+++ CH08/08_04/08_04-parray5.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
+
+int32_t main()
 {
-	int twos[5] = { 2, 4, 6, 8, 10 };
-	int *pt;
+	int32_t twos[5] = { 2, 4, 6, 8, 10 };
+	int32_t *pt;
 
 	pt = twos;
-	printf("%d\n",*(pt+0));
-	printf("%d\n",*(pt+1));
-	printf("%d\n",*(pt+2));
-	printf("%d\n",*(pt+3));
-	printf("%d\n",*(pt+4));
+	printf("%d\n", *(pt + 0));
+	printf("%d\n", *(pt + 1));
+	printf("%d\n", *(pt + 2));
+	printf("%d\n", *(pt + 3));
+	printf("%d\n", *(pt + 4));
 
-	return(0);
+	return EXIT_SUCCESS;
 }
-

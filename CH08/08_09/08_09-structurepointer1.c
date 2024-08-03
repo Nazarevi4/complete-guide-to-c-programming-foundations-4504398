f04190c502c4c4dc25ab7f521bb5dd6fc998642b
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-03 09:36:59 +0000

    08_09-structurepointer1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH08/08_09/08_09-structurepointer1.c
+++ CH08/08_09/08_09-structurepointer1.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 
-int main()
+
+int32_t main()
 {
 	struct person {
 		char *name;
-		int age;
+		int32_t age;
 	} you;
 
 	/* allocate storage */
-	you.name = malloc( sizeof(char) * 32);
-	if( you.name == NULL)
+	you.name = (char*)malloc(sizeof(char) * 32);
+	if( you.name == NULL )
 	{
 		puts("Unable to allocate storage");
 		exit(1);
@@ -19,15 +21,15 @@ int main()
 
 	/* gather input */
 	printf("Enter your name: ");
-	fgets(you.name,32,stdin);
+	fgets(you.name, 32, stdin);
 	printf("Enter your age: ");
-	scanf("%d",&you.age);
+	scanf("%d", &you.age);
 
-	printf("You are %s",you.name);
-	printf("You are %d years old\n",you.age);
+	printf("You are %s", you.name);
+	printf("You are %d years old\n", you.age);
 
 	/* clean-up */
 	free(you.name);
-	return(0);
-}
 
+	return EXIT_SUCCESS;
+}

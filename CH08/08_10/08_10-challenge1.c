793f95b440b816f64950fbf0ac6b679b10def904
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-03 10:05:04 +0000

    08_10-challenge1.c - Add the solution of the challenge

--- CH08/08_10/08_10-challenge1.c
+++ CH08/08_10/08_10-challenge1.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-int main()
+
+int32_t main()
 {
-	return 0;
+	struct Description {
+		int32_t page;
+		char letter;
+	} *book[26];
+	uint8_t k;
+
+	for(k = 0; k < 26; k++)
+	{
+		/* allocate storage */
+		book[k] = (struct Description*)malloc(sizeof(struct Description));
+		if( book[k] == NULL )
+		{
+			puts("Memory allocation error");
+			exit(1);
+		}
+
+		book[k]->page = k;
+		book[k]->letter = 'A' + k;
+	}
+
+	for(k = 0; k < 26; k++)
+	{
+		printf("%d, %c\n", book[k]->page, book[k]->letter);
+	}
+
+	for(k = 0; k < 26; k++)
+	{
+		free(book[k]);
+	}
+
+	return EXIT_SUCCESS;
 }

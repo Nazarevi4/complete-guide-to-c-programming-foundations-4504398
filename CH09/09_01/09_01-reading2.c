b10c0fca6d445a1a95415187fbfac579a67a59f1
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-03 15:08:02 +0000

    09_01-reading2.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH09/09_01/09_01-reading2.c
+++ CH09/09_01/09_01-reading2.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
+
+int32_t main()
 {
 	const char filename[] = "sonnet18.txt";
-	FILE *fh;
-	int ch;
+	FILE *fh = NULL;
+	int32_t ch;
 
 	/* open the file */
-	fh = fopen(filename,"r");
+	fh = fopen(filename, "r");
 	if( fh == NULL )
 	{
-		printf("Unable to read from file %s\n",filename);
+		printf("Unable to read from file %s\n", filename);
 		return(1);
 	}
-	
+
 	while(1)
 	{
 		ch = fgetc(fh);
-		if( ch == EOF )
-			break;
+		if( ch == EOF ) {	break; }
 		putchar(ch);
 	}
 
 	/* clean-up */
 	fclose(fh);
-	return(0);
+
+	return EXIT_SUCCESS;
 }

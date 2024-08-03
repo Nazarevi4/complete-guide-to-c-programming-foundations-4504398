5de7c3665ec2b70734c9a79bfaf96b7dbb548ce2
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-03 09:01:44 +0000

    08_07-allocate2.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH08/08_07/08_07-allocate2.c
+++ CH08/08_07/08_07-allocate2.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-int main()
+
+int32_t main()
 {
-	int *buffer;
-	int x;
+	int32_t *buffer;
 
 	/* allocate storage for 20 integers */
-	buffer = malloc( sizeof(int) * 20 );
+	buffer = (int32_t*)malloc(sizeof(int32_t) * 20);
 	if( buffer == NULL )
 	{
 		puts("Unable to allocate memory");
@@ -15,13 +16,14 @@ int main()
 	}
 
 	/* fill and display the buffer */
-	for(x=0;x<20;x++)
+	for(uint8_t x = 0; x < 20; x++)
 	{
-		*(buffer+x) = 10 * (x + 1);
-		printf("%d\n",*(buffer+x));
+		*(buffer + x) = 10 * (x + 1);
+		printf("%d\n", *(buffer + x));
 	}
 
 	/* exit */
 	free(buffer);
-	return(0);
+
+	return EXIT_SUCCESS;
 }

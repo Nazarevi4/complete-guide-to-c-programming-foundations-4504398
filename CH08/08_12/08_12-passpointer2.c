f71def3944a8a367a54db6f85b26bf9e10d5e452
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-03 14:30:45 +0000

    08_12-passpointer2.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH08/08_12/08_12-passpointer2.c
+++ CH08/08_12/08_12-passpointer2.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
 
 void three(float a, float *b, float *c)
 {
-	*b = a/3.0;
-	*c = a/7.0;
+	*b = a / 3.0;
+	*c = a / 7.0;
 }
 
-int main()
+
+int32_t main()
 {
-	float x,y;
+	float x, y;
 
-	three(20.0,&x,&y);
-	printf("20 / 3 = %.1f\n",x);
-	printf("20 / 7 = %.1f\n",y);
+	three(20.0, &x, &y);
+	printf("20 / 3 = %.1f\n", x);
+	printf("20 / 7 = %.1f\n", y);
 
-	return(0);
+	return EXIT_SUCCESS;
 }

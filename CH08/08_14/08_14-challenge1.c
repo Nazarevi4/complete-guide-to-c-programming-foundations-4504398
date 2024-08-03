ba16bf3d85ad4de657d734c78cd7d80b00393d50
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-03 14:52:20 +0000

    08_14-challenge1.c - Add the solution of the challenge

--- CH08/08_14/08_14-challenge1.c
+++ CH08/08_14/08_14-challenge1.c
@@ -1,18 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
 
 // write the get_input() function here
-
-int main()
+char *get_input(uint32_t size)
 {
-	char *your_name,*your_city;
+	/* Allocate storage */
+	char *input = (char*)malloc(sizeof(char) * size);
+	if( input == NULL )
+	{
+		puts("Unable to allocate memory");
+		exit(1);
+	}
+
+	/* Read input */
+	fgets(input, size, stdin);
+	uint32_t k = 0;
+
+	while( *(input + k) != '\0' )
+	{
+		if( *(input + k) == '\n' )
+		{
+			*(input + k) = '\0';
+			break;
+		}
+		k++;
+	}
+
+	return (input);
+}
+
+
+int32_t main()
+{
+	char *your_name = NULL;
+	char *your_city = NULL;
 
 	printf("Your name: ");
 	your_name = get_input(32);
 	printf("Your city: ");
 	your_city = get_input(32);
 
-	printf("%s lives in %s.\n",your_name,your_city);
+	printf("%s lives in %s.\n", your_name, your_city);
 
-	return 0;
+	free(your_name);
+	free(your_city);
+
+	return EXIT_SUCCESS;
 }

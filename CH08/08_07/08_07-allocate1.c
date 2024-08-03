75d8a481173224738817977bd8a1167f851d1cef
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-03 08:59:23 +0000

    08_07-allocate1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH08/08_07/08_07-allocate1.c
+++ CH08/08_07/08_07-allocate1.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-int main()
+
+int32_t main()
 {
 	char *buffer;
 
-	buffer = malloc( sizeof(char) * 128 );
+	buffer = (char*)malloc( sizeof(char) * 128 );
 	if( buffer == NULL )
 	{
 		puts("Unable to allocate memory");
 		exit(1);
 	}
+
 	puts("Buffer allocated");
 	free(buffer);
 	puts("Buffer freed");
 
-	return(0);
+	return EXIT_SUCCESS;
 }

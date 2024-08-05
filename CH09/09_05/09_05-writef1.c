67b7e643076e2f2befb7081dc7f180033d57c7ba
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-05 17:23:50 +0000

    09_05-writef1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH09/09_05/09_05-writef1.c
+++ CH09/09_05/09_05-writef1.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
+
+int32_t main()
 {
 	const char filename[] = "integers.dat";
 	FILE *fp;
-	int x,a;
+	int32_t a;
 
 	/* open and create the file */
-	fp = fopen(filename,"w");
+	fp = fopen(filename, "w");
 	if( fp == NULL )
 	{
-		printf("Unable to create %s\n",filename);
-		return(1);
+		printf("Unable to create %s\n", filename);
+		return (1);
 	}
 
 	/* write the integers */
 	a = 10;
-	for(x=0;x<10;x++)
+	for(uint8_t x = 0; x < 10; x++)
 	{
-		fwrite(&a, sizeof(int), 1, fp);
+		fwrite(&a, sizeof(int32_t), 1, fp);
 		a += 5;
 	}
 
 	/* close-up */
 	fclose(fp);
-	return(0);
-}
 
+	return EXIT_SUCCESS;
+}

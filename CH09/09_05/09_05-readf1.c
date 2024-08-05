2e00daa9153c0882b8b4dc0830a9cf94c2f12f9d
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-05 17:24:13 +0000

    09_05-readf1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic.

--- CH09/09_05/09_05-readf1.c
+++ CH09/09_05/09_05-readf1.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
+
+int32_t main()
 {
 	const char filename[] = "integers.dat";
 	FILE *fp;
-	int x,a;
+	int32_t a = 0;
 
 	/* open the file for reading */
-	fp = fopen(filename,"r");
+	fp = fopen(filename, "r");
 	if( fp == NULL )
 	{
-		printf("Unable to create %s\n",filename);
-		return(1);
+		printf("Unable to create %s\n", filename);
+		return (1);
 	}
 
 	/* read integers */
-	for(x=0;x<10;x++)
+	for(uint8_t x = 0; x < 10; x++)
 	{
-		fread(&a, sizeof(int), 1, fp );
-		printf("%d\n",a);
+		fread(&a, sizeof(int32_t), 1, fp );
+		printf("%d\n", a);
 	}
 
 	/* close */
 	fclose(fp);
 
-	return(0);
+	return EXIT_SUCCESS;
 }
-

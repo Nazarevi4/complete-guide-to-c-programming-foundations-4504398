b006f02945f9c5a469c627d3e11c9fa509132ece
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-06 09:29:15 +0000

    09_07-challenge1.c - Add the solution of the challenge

--- CH09/09_07/09_07-challenge1.c
+++ CH09/09_07/09_07-challenge1.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
+
+int32_t main()
 {
 	const char filename[] = "presidents.dat";
 	struct person {
 		char name[32];
-		int inauguration;
-		int age;
+		int32_t inauguration;
+		int32_t age;
 	} president;
-	int x;
+	int32_t record = 9;
 	FILE *fp;
 
 	/* open the file */
-	fp = fopen(filename,"r");
+	fp = fopen(filename, "r");
 	if( fp == NULL )
 	{
-		printf("Unable to open %s\n",filename);
+		printf("Unable to open %s\n", filename);
 		return(1);
 	}
 
 	/* read records from back to front */
+	while(record >= 0) {
+		fseek(fp, sizeof(struct person) * record, SEEK_SET);
+		record--;
+		/* fetch the record */
+		fread(&president, sizeof(struct person), 1, fp);
+		/* print the result */
+		printf("President %s was %d years old when inaugurated in %d\n",
+		    president.name,
+				president.age,
+				president.inauguration);
+	}
 
 	/* clean-up */
 	fclose(fp);
-	return(0);
+
+	return EXIT_SUCCESS;
 }
-
-

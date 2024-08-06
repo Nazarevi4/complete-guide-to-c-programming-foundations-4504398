c0cdd4c353bf4336a3a68fc75dd4c55ba597267f
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-06 09:28:19 +0000

    09_06-readrand1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic

--- CH09/09_06/09_06-readrand1.c
+++ CH09/09_06/09_06-readrand1.c
@@ -1,38 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
+
+int32_t main()
 {
 	const char filename[] = "presidents.dat";
 	struct person {
 		char name[32];
-		int inauguration;
-		int age;
+		int32_t inauguration;
+		int32_t age;
 	} president;
-	int x;
+	int32_t x;
 	FILE *fp;
 
 	/* open the file */
-	fp = fopen(filename,"r");
+	fp = fopen(filename, "r");
 	if( fp == NULL )
 	{
-		printf("Unable to open %s\n",filename);
-		return(1);
+		printf("Unable to open %s\n", filename);
+		return (1);
 	}
 
 	/* prompt to read a specific record */
 	printf("Enter record to read (1-10): ");
-	scanf("%d",&x);
+	scanf("%d", &x);
 	/* validate the input */
-	if( x<1 || x>10 )
+	if( x < 1 || x > 10 )
 	{
 		puts("Invalid input");
-		return(1);
+		return (1);
 	}
 	/* decrement x to reference the proper offset */
 	x--;
 
 	/* Set the file position indicator */
-	fseek(fp, sizeof(struct person)*x, SEEK_SET);
+	fseek(fp, sizeof(struct person) * x, SEEK_SET);
 	/* fetch the record */
 	fread(&president, sizeof(struct person), 1, fp);
 
@@ -40,12 +43,10 @@ int main()
 	printf("President %s was %d years old when inaugurated in %d\n",
 			president.name,
 			president.age,
-			president.inauguration
-		  );
+			president.inauguration);
 
 	/* close-up */
 	fclose(fp);
-	return(0);
+
+	return EXIT_SUCCESS;
 }
-
-

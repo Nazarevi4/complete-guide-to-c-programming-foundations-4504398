532a69c5ee44b3b6bf19e8f35d5c902833945b3f
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-08 15:38:40 +0000

    09_13-challenge1.c - Add the solution of the challenge

--- CH09/09_13/09_13-challenge1.c
+++ CH09/09_13/09_13-challenge1.c
@@ -1,20 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <unistd.h>
+
 
 void create_file(const char *f)
 {
+	/* Open/Create the file */
+	FILE *fh = fopen(f, "w");
+	if( fh == NULL)
+	{
+		printf("Failed to create '%s'\n", f);
+		exit(1);
+	}
+	printf("File '%s' created\n", f);
+
+	/* Write your name to the file */
+	fprintf(fh, "My name is John Wick");
+	puts("Data written");
+
+	/* Close the file */
+	fclose(fh);
+	printf("File '%s' closed\n", f);
 }
 
 void read_file(const char *f)
 {
+	char buffer[32];
+
+	/* Open/Read the file */
+	FILE *fh = fopen(f, "r");
+	if (fh == NULL)
+	{
+		printf("Failed to open '%s'\n", f);
+		exit(1);
+	}
+	printf("File '%s' opened\n", f);
+
+	/* Read your name from the file */
+	fgets(buffer, 32, fh);
+	printf("The file '%s' contains the text: %s\n",
+	    f,
+			buffer);
+
+	/* Close the file */
+	fclose(fh);
+	printf("File '%s' closed\n", f);
 }
 
 void delete_file(const char *f)
 {
+	if(unlink(f) == 0)
+	{
+		printf("File '%s' deleted\n", f);
+	}
+	else
+	{
+		printf("File '%s' NOT deleted\n", f);
+	}
 }
 
-int main()
+
+int32_t main()
 {
 	const char filename[] = "stuff.dat";
 
@@ -22,5 +69,5 @@ int main()
 	read_file(filename);
 	delete_file(filename);
 
-	return 0;
+	return EXIT_SUCCESS;
 }

93a37b46c4d38ed7cb140256e37ca813cc9a2963
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-08 16:28:24 +0000

    10_01-pow1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic

--- CH10/10_02/10_01-pow1.c
+++ CH10/10_02/10_01-pow1.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <math.h>
 
-int main()
+
+int32_t main()
 {
 	double a;
 
-	for(a=0.0;a<20.0;a+=1.0)
-		printf("%.0f\n",pow(2.0,a) );
+	for(a = 0.0; a < 20.0; a += 1.0)
+	{
+		printf("%.0f\n", pow(2.0, a));
+	}
 
-	return(0);
+	return EXIT_SUCCESS;
 }

1cd1bda99349e1ea05d1a20af937f39eed82f3de
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-09 19:23:24 +0000

    10_06/10_06-challenge1.c - Add the solution of the challenge

--- CH10/10_06/10_06-challenge1.c
+++ CH10/10_06/10_06-challenge1.c
@@ -1,19 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <time.h>
 
-int compare(const void *a, const void *b)
+
+int32_t compare(const void *a, const void *b)
 {
-	return( *(int *)a - *(int *)b);
+	return( *(int32_t *)a - *(int32_t *)b);
 }
 
-int main()
-{
-	// seed the randomizer
-	// allocate storage
-	// populate the grid with random values, 0 through 99
-	// quicksort the grid
-	// output the grid
 
-	return 0;
+int32_t main()
+{
+	const int32_t grow = 7;
+	const int32_t gcol = 10;
+	const int32_t gsize = grow * gcol;
+	int32_t *grid = NULL;
+	uint8_t x = 0;
+	
+	// seed the randomizer
+	srand((unsigned)time(NULL));
+	
+	// allocate storage
+	grid = (int32_t*)malloc(sizeof(int32_t) * gsize);
+	if( grid == NULL)
+	{
+		puts("Unable to allocate storage");
+		return 1;
+	}
+
+	// populate the grid with random values, 0 through 99
+	for(x = 0; x < gsize; x++ ) {
+		*(grid + x) = rand() % 100;
+	}
+
+	// quicksort the grid
+	qsort(grid, gsize, sizeof(int32_t), compare);
+
+	// output the grid
+	for(x = 0; x < grow; x++)
+	{
+		for(uint8_t y = 0; y < gcol; y++)
+		{
+			printf(" %2d ", *(grid + grow * y + x));
+		}
+		putchar('\n');
+	}
+
+	return EXIT_SUCCESS;
 }

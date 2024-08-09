984264fa4b0d76387d8055087cf86c8bfff88123
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-09 09:08:55 +0000

    10_02-time1.c - Add stdlib.h, stdint.h. Format the code without affecting the logic

--- CH10/10_03/10_02-time1.c
+++ CH10/10_03/10_02-time1.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <time.h>
 
-int main()
+
+int32_t main()
 {
 	time_t now;
 
 	time(&now);
-	printf("It's now %ld\n",now);
+	printf("It's now %ld\n", now);
 
-	return(0);
+	return EXIT_SUCCESS;
 }
-

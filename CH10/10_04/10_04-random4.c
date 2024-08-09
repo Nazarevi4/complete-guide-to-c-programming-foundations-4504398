fce8bed517865cfb61e4a94bb0a3da91694bd7b5
Ivan Nazarenko <vanyanazarenko1994@gmail.com>
2024-08-09 10:02:40 +0000

    10_04-random4.c - Add stdlib.h, stdint.h. Format the code without affecting the logic

--- CH10/10_04/10_04-random4.c
+++ CH10/10_04/10_04-random4.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <time.h>
 
-int main()
+
+int32_t main()
 {
-	int x,r;
+	int32_t r;
 
 	srand( (unsigned)time(NULL) );
 
-	for(x=0;x<100;x++)
+	for(uint8_t x = 0; x < 100; x++)
 	{
 		r = rand() % 100 + 1;
 		printf("%4d",r);
 	}
 	putchar('\n');
 
-	return(0);
+	return EXIT_SUCCESS;
 }
